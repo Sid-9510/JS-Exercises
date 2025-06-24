ebc4ed7dd81e90d6a7feb488b175e9120227fffe
Siddharth Chudasama <168630164+Sid-9510@users.noreply.github.com>
2025-06-24 06:34:30 +0000

    Exercises of 6 questions

---
+++ Strings_And_Array/Exercises.js
@@ -0,0 +1,50 @@
+/* 1) Write a JavaScript program to get the first n elements of an array. [n can be any positive number].
+For example: for array [7, 9, 0, -2, 5, -1, -7, 4] and n=3
+Print, [7, 9, 0] 
+*/
+// {
+//     let num = [7, 9, 0, -2, 5, -1, -7, 4];
+
+//     // In this question i have added some more functionality that the user can enter the staring number as well as ending number.
+
+//     let n1 = 1;
+
+//     let n2 = 4;
+
+//     let ans = num.slice(n1 - 1, n2);
+
+//     console.log(ans);
+// }
+
+
+/***************************************************************************************************/
+
+/* 2) Write a JavaScript program to get the last n elements of an array. [n can be any positive number].
+For example: for array [7, 9, 0, -2] and n=3
+Print, [9, 0, -2]
+ */
+// {
+
+//     let num = [7, 9, 0, -2, 5, -7, 1, 6];
+
+//     let n = 4; // Here we have tken 4 but we can also take input from user like :- prompt(`Numbers :- 7, 9, 0, -2, 5, -7, 1, 6 \n How many numbers you want from last?`);
+
+//     let ans = num.slice(-n);
+//     console.log(ans);
+// }
+
+/***************************************************************************************************/
+
+/* 3) Write a JavaScript program to check whether a string is blank or not. */
+
+// {
+//     let str = prompt("Enter a string");
+
+//     if (str.length == 0) {
+//         console.log(`The string is blank`);
+
+//     } else {
+//         console.log(`String is containing something`);
+
+//     }
+// }
